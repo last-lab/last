4d69fa46d261c54917a05650bafe5f231127b8e8
qcloud <ubuntu@localhost.localdomain>
2023-09-10 19:33:44 +0800

    modify label name

--- dashboard/resources.py
+++ dashboard/resources.py
@@ -382,11 +382,11 @@ class Animal(Dropdown):
 @app.register
 class DataManagePage(Dropdown):
     class LabelingPage(Model):
-        label = "label"
+        label = "Labeling"
         model = LabelPage
         filters = [filters.Search(name="task_type", label="Task Type")]
         fields = ["id", "task_type", "labeling_method", "release_time", "current_status"]
 
-    label = "DataManage"
+    label = "DataSet"
     icon = "fas fa-bars"
     resources = [LabelingPage]
