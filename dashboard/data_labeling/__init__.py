a3cd78985e5c3a2143f4d566b11fd7da02d3b4ef
dongxiaozhuang <dongxiaozhuang@pjlab.org.cn>
2023-09-12 17:28:21 +0800

    修复后台label config接口无法访问的问题

--- dashboard/data_labeling/__init__.py
+++ dashboard/data_labeling/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .labeling_router import router
+from last.services.app import app
 
 labeling_router = APIRouter()
 labeling_router.include_router(router)
+app.include_router(labeling_router)
