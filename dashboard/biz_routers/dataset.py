8e87731c75450c2b3c1f3a11f3a41feffd8dd802
fanhao <fanhao@pjlab.org.cn>
2023-09-15 12:58:34 +0800

    f- mock

--- dashboard/biz_routers/dataset.py
+++ dashboard/biz_routers/dataset.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, File, Path, UploadFile
 from jinja2 import TemplateNotFound
+from pydantic import BaseModel
 from starlette.requests import Request
 
 from dashboard.biz_models import DataSet
@@ -41,13 +42,38 @@ async def upload_dataset(
 
 @router.post("/dataset/json")
 async def json(request: Request, file: UploadFile = File(...)):
-    contents = await upload.upload(file)
+    # contents = await upload.upload(file)
+    contents = {
+        "result": 1,
+        "reason": "",
+        "focused_risks": [
+            {"level":1,"name":"国家安全","description":""},
+            {
+                "level":2,
+                "name":"颠覆政权",
+                "description":"",
+                "uplevel_risk_name": ["敏感信息", "安全问题"]
+            },
+            {
+                "level": 2,
+                "name": "宣扬恐怖主义",
+                "description": "",
+                "uplevel_risk_name": ["维度三1", "维度三2"]
+            },
+        ],
+        "qa_num": 666,
+        "word_cnt": 1000,
+        "volume": "10GB"
+    }
     return contents
 
 
-class Item(Dataset):
-    pass
-
+class Item(BaseModel):
+    name: str
+    focused_risks: str
+    volume: str
+    qa_num: str
+    word_cnt: str
 
 @router.post("/dataset/conform")
 async def conform(request: Request, item: Item):
