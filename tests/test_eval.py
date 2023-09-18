91ea41480f90f18bad0f8b21e4203748c80a8cd9
WangXuhongCN <wangxuhongcn@163.com>
2023-09-18 20:07:09 +0800

    start up

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,16 +1,22 @@
 # TODO client的部分、ORM的部分没有实现，LLM的调用是MOCK的
+import json
 import os
+import inspect
 
 from last.types.dataset import Dataset, QARecord
 from last.types.llm import LLM, LLMType
 from last.types.plan import EvaluationType, Plan
-from last.types.public import ID, RiskDimension
+from last.types.public import ID, RiskDimension, Placeholder
 from last.types.task import Task
+from last.client import AI_eval, Client
 
 
-def test_pipeline():
+#一个task需要plan+model，plan需要dataset
+
+def test_pipeline(datasets, llm_models, critic_model, plan, ):
     # 如果需要加载新数据集, 则提供file\url，返回数据集对象
-    file1_path = os.path.join("docs", "examples", "testset.csv")
+    file_paths = Placeholder(lambda x:x)
+    datasets = [dataset for file_path in file_paths]
     dataset1 = Dataset(
         name="test1",
         focused_risks=[RiskDimension(level=1, name="国家安全")],
@@ -20,7 +26,7 @@ def test_pipeline():
     dataset2 = Dataset(
         name="test2",
         focused_risks=[RiskDimension(level=1, name="个人隐私")],
-        file=file1_path,
+        file=file2_path,
     )
     # 明确评测方案，即使用哪些数据集合集进行评测
     plan = Plan(
@@ -60,3 +66,11 @@ def test_pipeline():
     )
 
     assert new_dataset is not None
+
+
+def test_pipeline():
+    kwargs_json = json.dumps(
+    {"$datasets": [{"name": "test1", "risks": {}, "file":os.path.join("docs", "examples", "testset1.csv")}, {"name": "test2", "risks": {}, "file":os.path.join("docs", "examples", "testset2.csv")}],
+     "$llm_models": }
+    )
+    Client.execute(foo_str, kwargs_json)
