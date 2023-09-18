92b8fe8d9f1fe7d5d57b386472a9a230c1902006
Eve Yu <512154936@qq.com>
2023-09-18 07:56:46 +0000

    按照comment更新字段

--- last/types/annotation_dataset.py
+++ last/types/annotation_dataset.py
@@ -4,13 +4,11 @@ from typing import Optional, Dict, List
 from .base import Record
 from .public import StateCode
 
-
-
-# 定义标注对象的数据的数据格式
+# 定义标注对象的数据的数据结构
 @dataclass
 class AnnotatedData(Record):
-    annotated_data_id: str  # 任务ID
-    anotated_data_type: str  # 标注对象的数据类型，text, image, video
+    annotated_data_id: str  # 标注数据集的id
+    anotated_data_types: Optional[List[str]]  # 标注数据集的数据类型，text, image, video
     content: str  # 标注对象的内容
     
  
@@ -19,26 +17,22 @@ class AnnotatedData(Record):
 class AnnotationTask(Record):
     annotation_task_id: str  # 任务ID
     annotated_data_id: str # 标注对象的数据ID
-    anotated_data_type: str  # 标注对象的数据类型，text, image, video
+    anotated_data_types: Optional[List[str]]  # 标注数据集的数据类型，text, image, video。上传数据集时应有的字段
     annotation_task_type: str  # 数据标注、审计标注
-    annotation_target: str  # 标注的目的，sentiment, answer
-    annotation_types: List[str]  # 标注方法的类型 0-1标注，box标注，rank标注等
-    label_contents: List[Dict]  # 标注需要的label内容
-    state: StateCode
+    annotation_method: List[str]  # 标注方法的类型 0-1标注，box标注，rank标注等
+    annotation_target: Optional[str]  # 标注的目的，sentiment, answer
+    label_contents: Optional[List[Dict]]  # 标注需要的label内容，目前直接前端固化流程
+    assigned_annotators: List[str]  # 分配给标注者的ID列表
+    assigned_auditors: List[str]  # 分配给审计者的ID列表
 
 
-# 定义单个标注结果的数据结构
+# 定义标注结果的数据结构
 @dataclass
 class AnnotationResult(Record):
     annotation_task_id: str  # 任务ID
-    annotation_sub_task_id: str  # 子任务ID，也就是本条annotation的id
-    annotation_type: str  # 标注方法的类型 0-1标注，box标注，rank标注等
-    annotated_data_id: str  # 标注对象的数据ID
-    value: Dict  # 标注的值，直接打包前端返回的annotation结果，格式参考AnnotationValue
-    origin: str  # 标注的来源（手动或自动）
-    complete_time: Optional[float]  # 完成标注所需的时间（秒），可选
-    sub_task_state: StateCode
-    # 在Record中的字段
+    annotation_value: Dict  # 结构体形式（底层为链表），直接打包前端返回的annotation结果。需要包括annotation_method, update_time, complete_time(完成标注所需的时间), annotation_origin(标注的来源（手动或自动）) 等字段
+    sub_task_state: StateCode  # 标注任务是否完成
+    # 在Record中的字段 
     # creator: UserInfo  # 标记者的id
     # created_at: DateString
     # updated_at:  Optional[DateString]
