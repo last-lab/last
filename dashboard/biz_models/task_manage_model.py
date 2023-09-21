1a0426073cb7517e1cf7c52c0da1b1894a65ea9b
Eve Yu <512154936@qq.com>
2023-09-21 09:09:44 +0000

    update LabelResult model

--- dashboard/biz_models/task_manage_model.py
+++ dashboard/biz_models/task_manage_model.py
@@ -7,3 +7,15 @@ class TaskManage(Model):
     dateset = fields.CharField(max_length=255)
     create_time = fields.DateField()
     current_status = fields.CharField(max_length=50)
+    
+class LabelResult(Model):
+    annotation_task_id = fields.CharField(max_length=50) # 任务ID
+    creator = fields.CharField(max_length=50) # 创建人
+    create_time = fields.DateField(max_length=50) 
+    update_time = fields.DateField(max_length=50)
+    annotation_value = fields.CharField(max_length=500)
+    sub_task_state = fields.CharField(max_length=50) # 标注任务是否完成
+    permission = fields.CharField(max_length=50) # 标注权限
+    
+    
+    
