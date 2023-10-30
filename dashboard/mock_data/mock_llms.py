1309daabf30d852041b0da227363cadc46663cea
WangXuhongCN <wangxuhongcn@163.com>
2023-10-30 14:57:15 +0800

    新增 wuya模型

--- dashboard/mock_data/mock_llms.py
+++ dashboard/mock_data/mock_llms.py
@@ -97,9 +97,9 @@ async def create_mock_llms():
         endpoint="alles-claude",
         access_key="None",
         secret_key="None",
-        model_type="聊天机器人、自然语言处理助手",
+        model_type="英文聊天机器人",
         version="3.5.0",
-        base_model="GPT3.5-Turbo",
+        base_model="",
         parameter_volume="约50亿",
         pretraining_info="包含约7500亿个英文和中文字词的大规模无标签文本数据集",
         finetuning_info="未经过微调",
@@ -123,6 +123,20 @@ async def create_mock_llms():
     await ModelInfo.create(
         name="alles-spark",
         endpoint="alles-spark",
+        access_key="None",
+        secret_key="None",
+        model_type="聊天机器人、自然语言处理助手",
+        version="3.5.0",
+        base_model="GPT3.5-Turbo",
+        parameter_volume="约50亿",
+        pretraining_info="包含约7500亿个英文和中文字词的大规模无标签文本数据集",
+        finetuning_info="未经过微调",
+        alignment_info="RLHF对齐",
+    )
+
+    await ModelInfo.create(
+        name="wuya",
+        endpoint="wuya",
         access_key="None",
         secret_key="None",
         model_type="聊天机器人、自然语言处理助手",
