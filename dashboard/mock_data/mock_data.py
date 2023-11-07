2615b1a2f1bfe7fa4bc26201a2c6abd5edaa4e9b
WangXuhongCN <wangxuhongcn@163.com>
2023-11-07 17:18:27 +0800

    因为字段修改，删除标注库

--- dashboard/mock_data/mock_data.py
+++ dashboard/mock_data/mock_data.py
@@ -1,24 +1,24 @@
 # 标注相关重新初始化
-# from .mock_audit_data import create_audit_mock_data
-# from .mock_labeling_data import create_labeling_mock_data
+from .mock_audit_data import create_audit_mock_data
+from .mock_labeling_data import create_labeling_mock_data
 from .mock_llms import create_mock_llms
 from .mock_risk import create_mock_risk
 
-# from .mock_task import create_task
+from .mock_task import create_task
 
 # 数据相关重新初始化
 # from .clean_old import clean_old_record
 # from .mock_dataset import create_mock_dataset
 
-# 评测报告重新初始画
+# 评测报告重新初始化
 # from .mock_report import create_mock_report
 # from .mock_risk_demo import create_mock_risk_demo
 
 
 async def create_mock_data():
-    # await create_labeling_mock_data()
-    # await create_task()
-    # await create_audit_mock_data()
+    await create_labeling_mock_data()
+    await create_task()
+    await create_audit_mock_data()
 
     await create_mock_risk()
     await create_mock_llms()
