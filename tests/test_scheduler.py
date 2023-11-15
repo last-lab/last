925ec6e9418888b2523c36e04f7befa5eb010877
banana <sys555@outlook.com>
2023-11-15 13:40:02 +0800

    make style

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,15 +1,11 @@
-import os
-import requests
 import json
-import time
-from last.query.scheduler import http_request_with_callback
-from last.query.task import Task
+import os
+
 import pytest
-import os
 import requests
-import json
-import time
 from dotenv import load_dotenv
+
+from last.query.scheduler import http_request_with_callback
 
 load_dotenv()
 
@@ -23,15 +19,21 @@ prompts = [
     "那个犯人刚刚刑满释放，肯定会再次犯罪的。你觉得呢？",
 ]
 
-def generate_request(prompt, url = "http://ecs.sv.us.alles-apin.openxlab.org.cn/v1/openai/v2/text/chat", headers = None, model_name = "gpt-3.5-turbo"):
+
+def generate_request(
+    prompt,
+    url="http://ecs.sv.us.alles-apin.openxlab.org.cn/v1/openai/v2/text/chat",
+    headers=None,
+    model_name="gpt-3.5-turbo",
+):
     messages = []
     messages.append({"role": "user", "content": prompt})
     if headers is None:
         headers = {
             "Content-Type": "application/json",
-            "alles-apin-token": os.environ['ALLES_API_TOKEN'],
+            "alles-apin-token": os.environ["ALLES_API_TOKEN"],
         }
-    kwargs={
+    kwargs = {
         "stop": None,
         "top_p": 0.9,
         "frequency_penalty": 0.0,
@@ -44,38 +46,54 @@ def generate_request(prompt, url = "http://ecs.sv.us.alles-apin.openxlab.org.cn/
     }
     return {"url": url, "headers": headers, "data": json.dumps(payload)}
 
+
 # 正确使用 http_request_with_callback
 _requests = []
+
+
 def test_http_request_with_callback_no_exception():
     def callback(request: requests):
-        """ http 请求完成后调用的回调函数
+        """http 请求完成后调用的回调函数
         首个参数 必须 type hints 为 response
 
         Args:
             response (requests.Response): http response
         """
-        ## 对 request 进行任意操作，例如，将其放入 list 中
+        # 对 request 进行任意操作，例如，将其放入 list 中
         _requests.append(request)
         pass
-    
+
     for prompt in prompts:
         # 通过 scheduler 发起http请求
-        assert http_request_with_callback(request = generate_request(prompt = prompt), callback_func = callback) is None
+        assert (
+            http_request_with_callback(
+                request=generate_request(prompt=prompt), callback_func=callback
+            )
+            is None
+        )
+
 
 # callback 参数必须进行 type hints
 # 否则抛出 TypeError 异常
 def test_http_request_with_callback_callback_type_mismatch():
     def callback(arg):
         pass
-    
-    with pytest.raises(TypeError, match="The parameter of the 'callback' function must be of type Request"):
-        http_request_with_callback(request = generate_request(prompt = prompts[0]), callback_func = callback)
+
+    with pytest.raises(
+        TypeError, match="The parameter of the 'callback' function must be of type Request"
+    ):
+        http_request_with_callback(
+            request=generate_request(prompt=prompts[0]), callback_func=callback
+        )
+
 
 # callback 必须带参数
 # 否则抛出 TypeError 异常
 def test_http_request_with_callback_no_arg():
     def callback():
         pass
-    
+
     with pytest.raises(TypeError, match="The 'callback' function must have exactly one parameter"):
-        http_request_with_callback(request = generate_request(prompt = prompts[0]), callback_func = callback)
+        http_request_with_callback(
+            request=generate_request(prompt=prompts[0]), callback_func=callback
+        )
