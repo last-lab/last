cad1ede84e8df332199bc183397007e99bb19b6f
陈亮 <PJLAB\chenliang@pjnl104220095l.pjlab.org>
2023-11-24 15:53:16 +0800

    delete old db

--- dashboard/mock_data/mock_data.py
+++ dashboard/mock_data/mock_data.py
@@ -1,16 +1,13 @@
 # 标注相关重新初始化
-# from .mock_audit_data import create_audit_mock_data
-# from .mock_labeling_data import create_labeling_mock_data
-
 # 数据相关重新初始化
 from .clean_old import clean_old_record
+from .mock_audit_data import create_audit_mock_data
+from .mock_labeling_data import create_labeling_mock_data
 
 # 系统必须的初始化
 from .mock_llms import create_mock_llms
 from .mock_risk import create_mock_risk
-
-# from .mock_task import create_task
-
+from .mock_task import create_task
 
 # from .mock_dataset import create_mock_dataset
 
@@ -21,9 +18,9 @@ from .mock_risk import create_mock_risk
 
 
 async def create_mock_data():
-    # await create_labeling_mock_data()
-    # await create_task()
-    # await create_audit_mock_data()
+    await create_labeling_mock_data()
+    await create_task()
+    await create_audit_mock_data()
 
     await create_mock_risk()
     await create_mock_llms()
