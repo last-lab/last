c37c5eb35ae8e6746b67aeea05255072b063d9e3
WangXuhongCN <wangxuhongcn@163.com>
2023-12-04 18:09:19 +0800

    修改评测需求

--- last/client/eval_funcs.py
+++ last/client/eval_funcs.py
@@ -1,12 +1,11 @@
 from last.types.dataset import Dataset, QARecord, Message, MessageRole
 from last.types.llm import LLM, LLMType
 from last.types.plan import EvaluationType, Plan
-from last.types.public import ID, Placeholder, RiskDimension
+from last.types.public import ID, Placeholder
 from last.types.task import Task
 from tqdm import tqdm
-import json
+import re
 import asyncio
-import multiprocessing
 
 
 async def AI_eval(
@@ -78,10 +77,11 @@ async def AI_eval(
     print(llm_model.name + "的评测进度:评判模型评测完毕")
 
     # 处理评测结果
-    for (dataset, qa_record), response, critic in zip(plan, response_list, critic_list):
+    for (dataset, qa_record), response, critic_str in zip(plan, response_list, critic_list):
         question = qa_record.question
         sheet_name = qa_record.sheet_name
-        new_qa_record = QARecord(sheet_name=sheet_name, question=question, answer=response, critic=critic)
+        critic, reason= parse_critic_and_reason(str(critic_str))
+        new_qa_record = QARecord(sheet_name=sheet_name, question=question, answer=response, critic=critic, reason=reason)
         new_qa_records[ID()] = new_qa_record
     progress_bar.update(1)
     print(llm_model.name + "的评测进度:评测结果处理完毕")
@@ -97,6 +97,22 @@ async def AI_eval(
     new_dataset = Dataset(name=plan.name + "+" + llm_model.name + "+问答记录", qa_records=new_qa_records, file=None)
 
     return task, new_dataset
+
+def parse_critic_and_reason(text: str):
+    reason = None
+    critic = None
+    reason_pattern = r"判断理由: “\s*(.*?)\s*”"
+    reason_match = re.search(reason_pattern, text)
+    if reason_match:
+        reason = reason_match.group(1)  
+
+    # 提取评判结果
+    result_pattern = r"判断结果: “\s*(.*?)\s”"
+    result_match = re.search(result_pattern, text)
+    if result_match:
+        critic = result_match.group(1)
+
+    return critic, reason
 
 class TaskList():
     """ 模拟任务队列 
